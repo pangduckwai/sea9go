953a9a214e68500e52555a5c79c5b65025c750a3
pangduckwai <paul.lai@sea9.org>
2026-03-27 15:07:08 +0800

    Improve http testing code

--- cmd/http/http.go
+++ cmd/http/http.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"net/http"
@@ -19,17 +21,41 @@ const FRM_LOGF_SEC = "2006-01-02_15:04:05"
 const PORT = 8999
 const IDLE = 30
 
+func hasher() (
+	hash func([]byte) (string, error),
+	hashString func(string) (string, error),
+	hashInt64 func(int64) (string, error),
+) {
+	hsh := sha256.New()
+
+	hash = func(v []byte) (h string, err error) {
+		hsh.Reset()
+		_, err = hsh.Write(v)
+		if err != nil {
+			return
+		}
+		h = hex.EncodeToString(hsh.Sum(nil))
+		return
+	}
+	hashString = func(v string) (string, error) {
+		return hash([]byte(v))
+	}
+	hashInt64 = func(v int64) (string, error) {
+		return hashString(strconv.FormatInt(v, 10))
+	}
+
+	return
+}
+
 // Usage: ./http [delay] [verbose] [num-of-runs]
 func main() {
-	lo, le, lf := logger.Init()
-	logsvr, errsvr := logger.AddPrefix(lo, "SERVER"), logger.AddPrefix(le, "SERVER")
-	logcln, fatal := logger.AddPrefix(lo, "CLIENT"), logger.AddPrefix(lf, "CLIENT")
-
 	var wg sync.WaitGroup
 	var err error
-	var run, tmp = 14, 0
+	var run, dgt, tmp = 14, 2, 0
 	vbs := false
 	dly := 0
+	lo, le, lf := logger.Init()
+
 	switch len(os.Args) {
 	case 4:
 		tmp, err = strconv.Atoi(os.Args[3])
@@ -41,6 +67,7 @@ func main() {
 		} else if tmp > 0 {
 			run = tmp
 		}
+		dgt = logger.DigitCount(uint64(run))
 		fallthrough
 	case 3:
 		vbs, err = strconv.ParseBool(os.Args[2])
@@ -66,6 +93,14 @@ func main() {
 			dly = IDLE + 1
 		}
 	}
+
+	logsvr, errsvr := logger.AddPrefix(lo, "SERVER"), logger.AddPrefix(le, "SERVER")
+	fatal := logger.AddPrefix(lf, "CLIENT")
+	logcln, _ := logger.AddLabels(logger.AddPrefix(lo, "CLIENT"), 2, 1, 2, dgt)
+	errcln, _ := logger.AddLabels(logger.AddPrefix(le, "CLIENT"), 2, 1, 2, dgt)
+	fatalc, _ := logger.AddLabels(fatal, 2, 1, 2, dgt)
+
+	_, hashString, hashInt64 := hasher()
 
 	/////////////////
 	// start server
@@ -94,6 +129,18 @@ func main() {
 				errsvr(" %v\n", err)
 				return
 			}
+			tk, err := hashString(ts)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				errsvr(" %v\n", err)
+				return
+			}
+			if tk != auth[0][7:] {
+				err = fmt.Errorf("authorization failed")
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+				errsvr(" %v\n", err)
+				return
+			}
 
 			env := r.PathValue("env")
 			if env == "" {
@@ -103,7 +150,8 @@ func main() {
 				return
 			}
 
-			_, err = fmt.Fprintf(w, "%v GET TEST1: ENV:%v TS:%v", time.Now().Format(FRM_LOGF_SEC), env, ts)
+			time.Sleep(100 * time.Millisecond)
+			_, err = fmt.Fprintf(w, "%v GET TEST1: TSTMP:%v TK:%v", time.Now().Format(FRM_LOGF_SEC), ts, tk)
 			if err != nil {
 				err = fmt.Errorf("[RES] %v", err)
 				http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -135,8 +183,21 @@ func main() {
 				errsvr(" %v\n", err)
 				return
 			}
+			tk, err := hashString(delay)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				errsvr(" %v\n", err)
+				return
+			}
+			if tk != auth[0][7:] {
+				err = fmt.Errorf("authorization failed")
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+				errsvr(" %v\n", err)
+				return
+			}
 
-			_, err = fmt.Fprintf(w, "%v GET TEST2: DELAY:%v", time.Now().Format(FRM_LOGF_SEC), delay)
+			time.Sleep(100 * time.Millisecond)
+			_, err = fmt.Fprintf(w, "%v GET TEST2: DELAY:%013v TK:%v", time.Now().Format(FRM_LOGF_SEC), delay, tk)
 			if err != nil {
 				err = fmt.Errorf("[RES] %v", err)
 				http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -165,7 +226,7 @@ func main() {
 	// prepare clients
 	client, err := client.Client(30 * time.Second)
 	if err != nil {
-		fatal("[CLNT]%v", err)
+		fatal("[NEW] %v", err)
 	}
 	if dly > 0 {
 		time.Sleep(time.Duration(dly) * time.Second) // delay 6 to 16 seconds
@@ -175,26 +236,38 @@ func main() {
 	// thread #1
 	wg.Add(1)
 	go func(client *http.Client) {
+		var err error
+		var ts int64
+		var tk string
+		var req *http.Request
+		var res *http.Response
+		var buf []byte
 		defer wg.Done()
-		for range run {
-			req, err := http.NewRequest("GET", fmt.Sprintf("http://localhost:%v/test1/TEST/%v", PORT, time.Now().Unix()), nil)
-			if err != nil {
-				fatal("[REQ][1][PREP]%v", err)
-			}
-			req.Header.Add("Authorization", "Bearer 1234567001")
+		for i := range run {
+			ts = time.Now().UnixMilli()
+			tk, err = hashInt64(ts)
 
-			res, err := client.Do(req)
+			req, err = http.NewRequest("GET", fmt.Sprintf("http://localhost:%v/test1/SHA2/%v", PORT, ts), nil)
 			if err != nil {
-				fatal("[RES][1][EXEC]%v", err)
+				fatalc("[REQ][PREP]%v", 1, i, err)
+			}
+			req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", tk))
+
+			res, err = client.Do(req)
+			if err != nil {
+				fatalc("[RES][EXEC]%v", 1, i, err)
 			}
 			defer res.Body.Close()
-			buf, err := io.ReadAll(res.Body)
+			buf, err = io.ReadAll(res.Body)
 			if err != nil {
-				fatal("[RES][1][READ]%v", err)
+				fatalc("[RES][READ]%v", 1, i, err)
 			}
 
-			logcln("[RES][1] %v\n", string(buf))
-			time.Sleep(350 * time.Millisecond)
+			if string(buf[54:]) == tk {
+				logcln("[RES] OKAY \"%v\"\n", 1, i, string(buf[:50]))
+			} else {
+				errcln("[RES] FAILED %v\n", 1, i, string(buf))
+			}
 		}
 	}(client)
 
@@ -202,26 +275,38 @@ func main() {
 	// thread #2
 	wg.Add(1)
 	go func(client *http.Client) {
+		var err error
+		var cnt int64
+		var tk string
+		var req *http.Request
+		var res *http.Response
+		var buf []byte
 		defer wg.Done()
 		for i := range run {
-			req, err := http.NewRequest("GET", fmt.Sprintf("http://localhost:%v/test2/%v", PORT, 900+i), nil)
-			if err != nil {
-				fatal("[REQ][2]%v", err)
-			}
-			req.Header.Add("Authorization", "Bearer 1234567002")
+			cnt = int64(i) + 900
+			tk, err = hashInt64(cnt)
 
-			res, err := client.Do(req)
+			req, err = http.NewRequest("GET", fmt.Sprintf("http://localhost:%v/test2/%v", PORT, cnt), nil)
 			if err != nil {
-				fatal("[RES][2][EXEC]%v", err)
+				fatalc("[REQ]%v", 2, i, err)
+			}
+			req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", tk))
+
+			res, err = client.Do(req)
+			if err != nil {
+				fatalc("[RES][EXEC]%v", 2, i, err)
 			}
 			defer res.Body.Close()
-			buf, err := io.ReadAll(res.Body)
+			buf, err = io.ReadAll(res.Body)
 			if err != nil {
-				fatal("[RES][2][READ]%v", err)
+				fatalc("[RES][READ]%v", 2, i, err)
 			}
 
-			logcln("[RES][2] %v\n", string(buf))
-			time.Sleep(350 * time.Millisecond)
+			if string(buf[54:]) == tk {
+				logcln("[RES] OKAY \"%v\"\n", 2, i, string(buf[:50]))
+			} else {
+				errcln("[RES] FAILED %v\n", 2, i, string(buf))
+			}
 		}
 	}(client)
 
