eb901af14fd75efdca2022b089d85f808b81b688
pangduckwai <paul.lai@sea9.org>
2026-05-05 19:55:15 +0800

    Fix illogical behaviour of 'error'

--- pkg/errors/error.go
+++ pkg/errors/error.go
@@ -8,40 +8,42 @@ import (
 
 // Err error type with the boolean property 'Fatal', also supports multiple errors.
 type Err struct {
-	Fatal  bool // Fatal severity level, 'true' -> fatal error, should stop further processing
-	Errors []error
+	fatal  bool // Fatal severity level, 'true' -> fatal error, should stop further processing
+	errors []error
 }
 
 func (e *Err) Error() string {
-	switch len(e.Errors) {
+	switch len(e.errors) {
 	case 0:
-		if e.Fatal {
+		if e.fatal {
 			return "Fatal error"
 		}
 		return "Error"
 	case 1:
-		msg := e.Errors[0].Error()
-		ftl := ""
-		if e.Fatal {
+		msg := e.errors[0].Error()
+		if e.fatal {
 			if msg == "" {
 				return "Fatal error"
-			}
-			if msg[0] != '[' {
-				ftl = "[FATAL] "
+			} else if msg[0] != '[' {
+				return fmt.Sprintf("[FATAL] %v", msg)
 			} else {
-				ftl = "[FATAL]"
+				return fmt.Sprintf("[FATAL]%v", msg)
 			}
 		}
-		return fmt.Sprintf("%v%v", ftl, msg)
+		return msg
 	default:
 		var sb strings.Builder
-		ftl := ""
-		if e.Fatal {
-			ftl = "[FATAL] "
+		if e.fatal {
+			fmt.Fprint(&sb, "[FATAL]:")
+		} else {
+			fmt.Fprint(&sb, "errors:")
 		}
-		fmt.Fprintf(&sb, "%verrors:\n - %v", ftl, e.Errors[0])
-		for _, err := range e.Errors[1:] {
-			fmt.Fprintf(&sb, "\n - %v", err)
+		for _, err := range e.errors {
+			if _, ok := err.(*Err); ok {
+				fmt.Fprintf(&sb, "\n %v.", err)
+			} else {
+				fmt.Fprintf(&sb, "\n %v", err)
+			}
 		}
 		return sb.String()
 	}
@@ -49,10 +51,10 @@ func (e *Err) Error() string {
 
 func New(fatal bool, errs ...string) (r *Err) {
 	r = &Err{
-		Fatal: fatal,
+		fatal: fatal,
 	}
 	for _, err := range errs {
-		r.Errors = append(r.Errors, errors.New(err))
+		r.errors = append(r.errors, errors.New(err))
 	}
 	return
 }
@@ -62,85 +64,91 @@ func Count(err error) int {
 		return 0
 	}
 	if e, ok := err.(*Err); ok {
-		return len(e.Errors)
+		return len(e.errors)
 	}
 	return 1
 }
 
 func IsFatal(err error) bool {
 	if e, ok := err.(*Err); ok {
-		return e.Fatal // for error type Err, check the e.Fatal flag
+		return e.fatal // for error type Err, check the e.Fatal flag
 	}
 	return true
 }
 
 func Fatal(err string) *Err {
 	return &Err{
-		Fatal:  true,
-		Errors: []error{errors.New(err)},
+		fatal:  true,
+		errors: []error{errors.New(err)},
 	}
 }
 
 func Fatalf(format string, a ...any) *Err {
 	return &Err{
-		Fatal:  true,
-		Errors: []error{fmt.Errorf(format, a...)},
+		fatal:  true,
+		errors: []error{fmt.Errorf(format, a...)},
 	}
 }
 
 func NonFatal(err string) *Err {
 	return &Err{
-		Fatal:  false,
-		Errors: []error{errors.New(err)},
+		fatal:  false,
+		errors: []error{errors.New(err)},
 	}
 }
 
 func NonFatalf(format string, a ...any) *Err {
 	return &Err{
-		Fatal:  false,
-		Errors: []error{fmt.Errorf(format, a...)},
+		fatal:  false,
+		errors: []error{fmt.Errorf(format, a...)},
 	}
 }
 
 // Append append new errors in 'errs' to 'base'.
 // - if 'base' is of type *Err, 'Fatal' in 'base' will be used.
 // - if 'base' is not of type *Err:
-//   - 'base' will first be converted to *Err with 'Fatal' default to false.
-//   - if 'errs' is not empty and the first error in 'errs' is of type *Err, the 'Fatal' value in the first error will be used.
+//   - 'base' will first be converted to *Err with 'Fatal' default to true.
+//   - if 'errs' is not empty, the 'Fatal' value of the first non-empty error in 'errs' of type *Err will be used.
 func Append(base error, errs ...error) (r *Err) {
-	var isErr bool
-	if base != nil {
-		if e, ok := base.(*Err); !ok {
-			r = &Err{ // default is non-fatal
-				Errors: []error{base},
+	var set bool
+	if e, ok := base.(*Err); !ok {
+		if base != nil && base.Error() != "" {
+			r = &Err{ // default is fatal
+				fatal:  true,
+				errors: []error{base},
 			}
-		} else {
-			isErr = true
-			r = e
 		}
+	} else if e != nil {
+		set = true
+		r = e
 	}
 
 	if len(errs) > 0 {
 		if r == nil {
-			r = &Err{} // default is non-fatal
+			r = &Err{
+				fatal: true,
+			} // default is fatal
 		}
-		if !isErr { // if is *Err, err.Fatal follows the value in base
-			if e, ok := errs[0].(*Err); ok {
-				r.Fatal = e.Fatal
-			}
-		}
-		// err.Errors = append(err.Errors, errs...)
 		for _, err := range errs {
 			if e, ok := err.(*Err); ok {
-				r.Errors = append(r.Errors, e.Errors...)
+				if e != nil && len(e.errors) > 0 {
+					if !set {
+						r.fatal = e.fatal
+						set = true
+					}
+					r.errors = append(r.errors, e.errors...) // flatten nested *Err
+				}
 			} else {
-				r.Errors = append(r.Errors, err)
+				if err != nil && err.Error() != "" {
+					r.errors = append(r.errors, err)
+				}
 			}
 		}
 	}
 	return
 }
 
+// Appendf append a new error created by fmt.Errorf().
 func Appendf(base error, format string, a ...any) *Err {
 	return Append(base, fmt.Errorf(format, a...))
 }
--- pkg/errors/error_test.go
+++ pkg/errors/error_test.go
@@ -6,43 +6,51 @@ import (
 	"testing"
 )
 
+func TestBase(t *testing.T) {
+	var err error = errors.New("error!")
+	if !IsFatal(err) {
+		t.Fatalf("TestBase() expected fatal error but got non-fatal")
+	}
+	fmt.Printf("TestBase() \"%v\"\n", err)
+}
+
 func TestError(t *testing.T) {
 	var err0 error
 	var err1 error = errors.New("error 1")
 	err := Append(err0, err1)
-	if len(err.Errors) != 1 {
-		t.Fatalf("TestError() expected 1 error but got %v", len(err.Errors))
+	if Count(err) != 1 {
+		t.Fatalf("TestError() expected 1 error but got %v", Count(err))
 	}
-	if err.Fatal {
-		t.Fatalf("TestError() expected non-fatal error but got fatal")
+	if !IsFatal(err) {
+		t.Fatalf("TestError() expected fatal error but got non-fatal")
 	}
 	fmt.Printf("TestError() \"%v\"\n", err)
 }
 
-func TestFatal(t *testing.T) {
+func TestNonFatal(t *testing.T) {
 	var err0 error
-	var err1 error = Fatal("error 1")
+	var err1 error = NonFatal("error 1")
 	err := Append(err0, err1)
-	if len(err.Errors) != 1 {
-		t.Fatalf("TestFatal() expected 1 error but got %v", len(err.Errors))
+	if Count(err) != 1 {
+		t.Fatalf("TestNonFatal() expected 1 error but got %v", Count(err))
 	}
-	if !err.Fatal {
-		t.Fatalf("TestFatal() expected fatal error but got non-fatal")
+	if IsFatal(err) {
+		t.Fatalf("TestNonFatal() expected non-fatal error but got fatal")
 	}
-	fmt.Printf("TestFatal() \"%v\"\n", err)
+	fmt.Printf("TestNonFatal() \"%v\"\n", err)
 }
 
 func TestChain(t *testing.T) {
 	var err0 error
 	var err1 error = Fatal("[CHAIN] error 1")
 	err := Append(err0, err1)
-	if len(err.Errors) != 1 {
-		t.Fatalf("TestChain() expected 1 error but got %v", len(err.Errors))
+	if Count(err) != 1 {
+		t.Fatalf("TestChain() expected 1 error but got %v", Count(err))
 	}
-	if !err.Fatal {
+	if !IsFatal(err) {
 		t.Fatalf("TestChain() expected fatal error but got non-fatal")
 	}
-	fmt.Printf("TestChain() \"%v\"\n", err)
+	fmt.Printf("TestChain() \"%v\"\n\n", err)
 }
 
 func TestErrors(t *testing.T) {
@@ -50,41 +58,41 @@ func TestErrors(t *testing.T) {
 	var err1 error = errors.New("error 1")
 	var err2 error = errors.New("error 2")
 	err := Append(err0, err1, err2)
-	if len(err.Errors) != 2 {
-		t.Fatalf("TestErrors() expected 2 errors but got %v", len(err.Errors))
+	if Count(err) != 2 {
+		t.Fatalf("TestErrors() expected 2 errors but got %v", Count(err))
 	}
-	if err.Fatal {
-		t.Fatalf("TestErrors() expected non-fatal error but got fatal")
+	if !IsFatal(err) {
+		t.Fatalf("TestErrors() expected fatal error but got non-fatal")
 	}
-	fmt.Printf("TestErrors():\n%v\n", err)
+	fmt.Printf("TestErrors():\n%v\n\n", err)
 }
 
 func TestWithBase(t *testing.T) {
-	err0 := Fatal("fatal error 0")
+	err0 := NonFatal("error 0")
 	var err1 error = errors.New("error 1")
 	var err2 error = errors.New("error 2")
 	err := Append(err0, err1, err2)
-	if len(err.Errors) != 3 {
-		t.Fatalf("TestWithBase() expected 3 errors but got %v", len(err.Errors))
+	if Count(err) != 3 {
+		t.Fatalf("TestWithBase() expected 3 errors but got %v", Count(err))
 	}
-	if !err.Fatal {
-		t.Fatalf("TestWithBase() expected fatal error but got non-fatal")
+	if IsFatal(err) {
+		t.Fatalf("TestWithBase() expected non-fatal error but got fatal")
 	}
-	fmt.Printf("TestWithBase()\n%v\n", err)
+	fmt.Printf("TestWithBase()\n%v\n\n", err)
 }
 
 func TestErr(t *testing.T) {
 	var err0 error
-	var err1 error = Fatal("error 1")
+	var err1 error = NonFatal("error 1")
 	var err2 error = errors.New("error 2")
 	err := Append(err0, err1, err2)
-	if len(err.Errors) != 2 {
-		t.Fatalf("TestErr() expected 2 errors but got %v", len(err.Errors))
+	if Count(err) != 2 {
+		t.Fatalf("TestErr() expected 2 errors but got %v", Count(err))
 	}
-	if !err.Fatal {
-		t.Fatalf("TestErr() expected fatal error but got non-fatal")
+	if IsFatal(err) {
+		t.Fatalf("TestErr() expected non-fatal error but got fatal")
 	}
-	fmt.Printf("TestErr()\n%v\n", err)
+	fmt.Printf("TestErr()\n%v\n\n", err)
 }
 
 func TestNil(t *testing.T) {
@@ -92,39 +100,110 @@ func TestNil(t *testing.T) {
 	var err1 error = NonFatal("error 1")
 	var err2 error = errors.New("error 2")
 	err := Append(err0, err1, err2)
-	if len(err.Errors) != 2 {
-		t.Fatalf("TestNil() expected 2 errors but got %v", len(err.Errors))
+	if Count(err) != 2 {
+		t.Fatalf("TestNil() expected 2 errors but got %v", Count(err))
 	}
-	if err.Fatal {
+	if IsFatal(err) {
 		t.Fatalf("TestNil() expected non-fatal error but got fatal")
 	}
-	fmt.Printf("TestNil()\n%v\n", err)
+	fmt.Printf("TestNil()\n%v\n\n", err)
 }
 
-func TestFatals(t *testing.T) {
-	err0 := New(true)
+func TestNils(t *testing.T) {
+	var err0 error = NonFatal("error 0")
+	var err1 *Err
+	var err2 error = errors.New("error 2")
+	err := Append(err0, err1, err2)
+	if Count(err) != 2 {
+		t.Fatalf("TestNils() expected 2 errors but got %v", Count(err))
+	}
+	if IsFatal(err) {
+		t.Fatalf("TestNils() expected non-fatal error but got fatal")
+	}
+	fmt.Printf("TestNils()\n%v\n\n", err)
+}
+
+func TestNilss(t *testing.T) {
+	var err0 error = NonFatal("error 0")
+	var err1 error
+	var err2 error = errors.New("error 2")
+	err := Append(err0, err1, err2)
+	if Count(err) != 2 {
+		t.Fatalf("TestNilss() expected 2 errors but got %v", Count(err))
+	}
+	if IsFatal(err) {
+		t.Fatalf("TestNilss() expected non-fatal error but got fatal")
+	}
+	fmt.Printf("TestNilss()\n%v\n\n", err)
+}
+
+func TestPlains(t *testing.T) {
+	var err0 *Err
 	var err1 error = errors.New("error 1")
 	var err2 error = errors.New("error 2")
 	err := Append(err0, err1, err2)
-	if len(err.Errors) != 2 {
-		t.Fatalf("TestFatals() expected 2 errors but got %v", len(err.Errors))
+	if Count(err) != 2 {
+		t.Fatalf("TestPlains() expected 2 errors but got %v", Count(err))
 	}
-	if !err.Fatal {
-		t.Fatalf("TestFatals() expected fatal error but got non-fatal")
+	if !IsFatal(err) {
+		t.Fatalf("TestPlains() expected fatal error but got non-fatal")
 	}
-	fmt.Printf("TestFatals()\n%v\n", err)
+	fmt.Printf("TestPlains()\n%v\n\n", err)
 }
 
-func TestFatalss(t *testing.T) {
-	err0 := New(true, "error 0", "error 3")
+func TestNonFatals(t *testing.T) {
+	err0 := New(false)
 	var err1 error = errors.New("error 1")
 	var err2 error = errors.New("error 2")
 	err := Append(err0, err1, err2)
-	if len(err.Errors) != 4 {
-		t.Fatalf("TestFatalss() expected 4 errors but got %v", len(err.Errors))
+	if Count(err) != 2 {
+		t.Fatalf("TestNonFatals() expected 2 errors but got %v", Count(err))
 	}
-	if !err.Fatal {
-		t.Fatalf("TestFatalss() expected fatal error but got non-fatal")
+	if IsFatal(err) {
+		t.Fatalf("TestNonFatals() expected non-fatal error but got fatal")
 	}
-	fmt.Printf("TestFatalss()\n%v\n", err)
+	fmt.Printf("TestNonFatals()\n%v\n\n", err)
+}
+
+func TestNonFatalss(t *testing.T) {
+	err0 := New(false, "error 0", "error 3")
+	var err1 error = errors.New("error 1")
+	var err2 error = errors.New("error 2")
+	err := Append(err0, err1, err2)
+	if Count(err) != 4 {
+		t.Fatalf("TestNonFatalss() expected 4 errors but got %v", Count(err))
+	}
+	if IsFatal(err) {
+		t.Fatalf("TestNonFatalss() expected non-fatal error but got fatal")
+	}
+	fmt.Printf("TestNonFatalss()\n%v\n\n", err)
+}
+
+func TestErrs(t *testing.T) {
+	var err0 error = errors.New("error 0")
+	err1 := New(false, "error 1", "error 3")
+	var err2 error = errors.New("error 2")
+	err := Append(err0, err1, err2)
+	if Count(err) != 4 {
+		t.Fatalf("TestErrs() expected 4 errors but got %v", Count(err))
+	}
+	if IsFatal(err) {
+		t.Fatalf("TestErrs() expected non-fatal error but got fatal")
+	}
+	fmt.Printf("TestErrs()\n%v\n\n", err)
+}
+
+func TestPackage(t *testing.T) {
+	err0 := New(false, "yo!")
+	var err1 error = errors.New("error 1")
+	var err2 error = NonFatal("error 2")
+	err := Append(err0, err1)
+	err.errors = append(err.errors, err2)
+	if Count(err) != 3 {
+		t.Fatalf("TestErrs() expected 3 errors but got %v", Count(err))
+	}
+	if IsFatal(err) {
+		t.Fatalf("TestErrs() expected non-fatal error but got fatal")
+	}
+	fmt.Printf("TestErrs()\n%v\n\n", err)
 }
